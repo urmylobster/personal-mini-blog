84707e656fd8041f68549d62f97ed1f1581eff01
limingxin <limingxin0729@foxmail.com>
2020-07-21 21:08:50 +0800

    ADD:命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,6 @@
 	<a href="https://learnku.com/laravel/courses" target="_blank">刻意练习,每日精进</a>
 
 	<div class="float-right">
-		<a href="/about">关于</a>
+		<a href="{{ route('about') }}">关于</a>
 	</div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a href="/" class="navbar-brand">微博APP</a>
+        <a href="{{ route('home') }}" class="navbar-brand">微博APP</a>
         <ul class="navbar-nav justify-content-end">
             <li class="nav-item">
-                <a href="/help" class="nav-link">帮助</a>
+                <a href="{{ route('help') }}" class="nav-link">帮助</a>
             </li>
             <li class="nav-item">
                 <a href="#" class="nav-link">登录</a>
