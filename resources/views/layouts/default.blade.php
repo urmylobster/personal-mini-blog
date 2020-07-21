fef87c9cde9a00364222f4473b44a30322a1fc1b
limingxin <limingxin0729@foxmail.com>
2020-07-21 19:35:33 +0800

    MOD:初始化boostrap样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,24 @@
 <html>
   <head>
     <title>@yield('title','Weibo App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">微博APP</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item">
+                    <a href="/help" class="nav-link">帮助</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">登录</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  	<div class="container">
+	    @yield('content')	  		
+  	</div>
   </body>
 </html>
