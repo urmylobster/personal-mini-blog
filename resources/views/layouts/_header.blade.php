ff6c79c5946e44c06413fbaa5abce5e5f390d7fb
limingxin <limingxin0729@foxmail.com>
2020-07-21 20:59:38 +0800

    MOD:切割头部和尾部子视图

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">微博APP</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item">
+                <a href="/help" class="nav-link">帮助</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">登录</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,21 +5,10 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a href="/" class="navbar-brand">微博APP</a>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item">
-                    <a href="/help" class="nav-link">帮助</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">登录</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    @include('layouts._header')
   	<div class="container">
-	    @yield('content')	  		
+	    @yield('content')	  	
+      @include('layouts._footer')	
   	</div>
   </body>
 </html>
