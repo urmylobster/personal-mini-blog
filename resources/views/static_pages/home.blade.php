3b57d269263f3af3e56a35126fffe3bc32f1814d
limingxin <limingxin0729@foxmail.com>
2020-07-21 21:38:35 +0800

    ADD:增加用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -11,6 +11,9 @@
     <p>
       <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
     </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
   </div>
 @stop
 
