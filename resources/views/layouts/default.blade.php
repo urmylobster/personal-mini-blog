130f989cae682ced070b4d0f88eef913a3ebab21
limingxin <limingxin0729@foxmail.com>
2020-07-22 17:08:30 +0800

    ADD:完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,8 +7,11 @@
   <body>
     @include('layouts._header')
   	<div class="container">
-	    @yield('content')	  	
-      @include('layouts._footer')	
+      <div class="offset-md-1 col-md-10">
+      @include('shared._messages')
+      @yield('content')     
+      @include('layouts._footer')
+      </div>	
   	</div>
   </body>
 </html>
